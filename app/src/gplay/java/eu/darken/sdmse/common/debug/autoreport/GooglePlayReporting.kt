67646fd1c8650f6efb4719be6ad8c040fc0c9008
darken <darken@darken.eu>
2025-06-06 21:01:46 +0200

    Refactor: Adjust GooglePlayReporting import path
    This commit updates the import path for `GooglePlayReporting` within `DebugModule.kt`.
    
    The class `GooglePlayReporting` itself has been moved from the package `eu.darken.butler.common.debug.autoreport` to `eu.darken.sdmse.common.debug.autoreport`.
    
    Additionally, within `GooglePlayReporting.kt`:
    - The import for `SDMId` has been changed to `ButlerId`.
    - The `setup()` method logging has been changed from displaying the enabled state to "setup(): noop", reflecting its current no-operation status.

--- app/src/gplay/java/eu/darken/sdmse/common/debug/autoreport/GooglePlayReporting.kt
+++ app/src/gplay/java/eu/darken/sdmse/common/debug/autoreport/GooglePlayReporting.kt
@@ -1,10 +1,9 @@
-package eu.darken.butler.common.debug.autoreport
+package eu.darken.sdmse.common.debug.autoreport
 
 import android.app.Application
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
-import eu.darken.butler.common.SDMId
-import eu.darken.butler.common.datastore.valueBlocking
+import eu.darken.butler.common.ButlerId
 import eu.darken.butler.common.debug.AutomaticBugReporter
 import eu.darken.butler.common.debug.DebugSettings
 import eu.darken.butler.common.debug.logging.log
@@ -18,12 +17,11 @@ class GooglePlayReporting @Inject constructor(
     @ApplicationContext private val context: Context,
     private val generalSettings: GeneralSettings,
     private val debugSettings: DebugSettings,
-    private val sdmId: SDMId,
+    private val butlerId: ButlerId,
 ) : AutomaticBugReporter {
 
     override fun setup(application: Application) {
-        val isEnabled = generalSettings.isBugReporterEnabled.valueBlocking
-        log(TAG) { "setup(): isEnabled=$isEnabled" }
+        log(TAG) { "setup(): noop" }
 
         // NOOP
     }
