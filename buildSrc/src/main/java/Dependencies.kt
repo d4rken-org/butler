d776f9926ec036ba0619f6b9f65db4e5a64e04c6
darken <darken@darken.eu>
2025-08-11 11:47:04 +0200

    Refactor: Migrate Room from KAPT to KSP
    Updated `Dependencies.kt` to use KSP instead of KAPT for the Room compiler dependency.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -126,7 +126,7 @@ fun DependencyHandlerScope.addAndroidCore() {
 fun DependencyHandlerScope.addRoomDb() {
     implementation("androidx.room:room-runtime:2.7.0")
     implementation("androidx.room:room-ktx:2.7.0")
-    kapt("androidx.room:room-compiler:2.7.0")
+    ksp("androidx.room:room-compiler:2.7.0")
 }
 
 fun DependencyHandlerScope.addWorkerManager() {
