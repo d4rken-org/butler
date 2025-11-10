d8afd0c066bff172befd9c838b91bf206a590817
darken <darken@darken.eu>
2025-11-10 17:11:43 +0100

    chore: Remove temporary workspace auto-creation
    This removes the test code that automatically created a workspace if none existed.

--- app/src/main/java/eu/darken/butler/workspace/ui/workspaces/WorkspacesViewModel.kt
+++ app/src/main/java/eu/darken/butler/workspace/ui/workspaces/WorkspacesViewModel.kt
@@ -20,7 +20,6 @@ import eu.darken.butler.workspace.core.WorkspaceSettings
 import eu.darken.butler.workspace.ui.WorkspacePageManager
 import eu.darken.butler.workspace.ui.WorkspacePanelMode
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
@@ -43,15 +42,6 @@ class WorkspacesViewModel @Inject constructor(
     private val hiddenMotdIds = MutableStateFlow<Set<Uuid>>(emptySet())
 
     init {
-        launch {
-            val currentWorkspaces = workspaceRepo.state.first()
-            if (currentWorkspaces.infos.isEmpty()) {
-                log(tag) { "No workspaces found, auto-creating workspace for testing" }
-                // FIXME: AUTO-CREATE WORKSPACE FOR TESTING - REMOVE BEFORE MERGE, DO NOT COMMIT
-                workspaceRepo.execute(WorkspaceAction.Create(type = Workspace.Type.EDITOR))
-            }
-        }
-
         // Initialize the WorkspaceUIManager with saved state
         workspacePageManager.initializeFromSavedState(savedStateHandle)
 
