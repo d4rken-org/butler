ee987476104ac0c04e303a83af2b1f1a103a30b3
darken <darken@darken.eu>
2025-12-25 15:27:47 +0100

    refactor(settings): Remove unused search settings from EditorSettings
    Delete obsolete search-related settings (case sensitive, regex, wrap around, max results) from the editor's preferences.

--- app-workspace-editor/src/main/java/eu/darken/butler/editor/core/EditorSettings.kt
+++ app-workspace-editor/src/main/java/eu/darken/butler/editor/core/EditorSettings.kt
@@ -32,12 +32,6 @@ class EditorSettings @Inject constructor(
     val autoSaveInterval = dataStore.createValue("editor.auto_save.interval_ms", 30000L) // 30 seconds
     val autoSaveEnabled = dataStore.createValue("editor.auto_save.enabled", false)
 
-    // Search Settings
-    val searchCaseSensitive = dataStore.createValue("editor.search.case_sensitive", false)
-    val searchRegex = dataStore.createValue("editor.search.regex", false)
-    val searchWrapAround = dataStore.createValue("editor.search.wrap_around", true)
-    val maxSearchResults = dataStore.createValue("editor.search.max_results", 1000)
-
     // Editor Behavior
     val autoIndent = dataStore.createValue("editor.auto_indent", true)
     val highlightCurrentLine = dataStore.createValue("editor.highlight.current_line", true)
@@ -57,12 +51,6 @@ class EditorSettings @Inject constructor(
         // File Handling Settings
         autoSaveInterval,
         autoSaveEnabled,
-
-        // Search Settings
-        searchCaseSensitive,
-        searchRegex,
-        searchWrapAround,
-        maxSearchResults,
 
         // Editor Behavior
         autoIndent,
